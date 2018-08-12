42faa162e849d75e2ca0c3112a16db7eb2ab14a9
Ana Mafla <anamafla@hotmail.com>
2018-08-12 16:32:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ function placeOrder(cardNumber) {
   if (cardNumber){
     
   } else {
-    return ("Sorry, we don't have a credit card on file for you")
+    return ("Sorry, we don't have a credit card on file for you.")
   }
 }
