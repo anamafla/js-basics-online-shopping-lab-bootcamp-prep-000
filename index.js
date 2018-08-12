4fbca8e719b16ad45b6a99b0ef23fe25fcf72beb
Ana Mafla <anamafla@hotmail.com>
2018-08-12 16:08:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function removeFromCart(item) {
   if(itemFound === false)
    return("That item is not in your cart")
   } else {
-    return("That item is not in your cart")
+    return("That item is not in your cart.")
   }
   
   
